c8a3431e1bcb11856170e92202f57e233255c3c1
harin0707 <yuharin0707@gmail.com>
2022-10-30 02:14:56 +0900

    메인페이지에 이미지

--- src/page/Page.js
+++ src/page/Page.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import {PageContainer, Img, Desc} from '../styled/styled.js'
+import jeju from '../data/jeju.JPG';
 
-//
+// val(버튼 눌렀는지 여부)==false -> main 페이지
+// val == true -> 그림이미지와 설명
 
 const Page = ({image, author, name, material, year, val}) => {
     return (
         <PageContainer>
             {val === false 
-            ? <div> 🏝환영합니다🏝 </div>
+            ? 
+            <>
+                <img src={jeju} alt='제주사진'/>
+                <div> 🏝환영합니다🏝 </div>
+            </>
             :
             <>
             <Img src={image} alt="img"/>
