c351076cd6cfc4b0551a7a877b092192a030bf52
harin0707 <yuharin0707@gmail.com>
2022-11-23 02:04:17 +0900

    refactor: kakao share

--- src/component/KakaoButton.js
+++ src/component/KakaoButton.js
@@ -2,48 +2,35 @@ import React from 'react'
 import styled from 'styled-components'
 
 export const KakaoButton = () => {
+    //자바스크립트 SDK로 카카오 api 호출
+    //카카오 api가 정상적으로 넘어온 것을 확인 가능
+    const kakao = window.Kakao;
+    const kakaoJsKey = process.env.REACT_APP_KAKAO_KEY;
+    const template = process.env.REACT_APP_MESSAGE_TEMPLATE;
+
     
-
-    const KakaoLink = ()=>{
-        //자바스크립트 SDK로 카카오 api 호출
-
-        //카카오 api가 정상적으로 넘어온 것을 확인 가능
-        const kakao = window.Kakao;
-        console.log(kakao);
-
-        //중복 initialization 방지
-        if (!kakao.isInitialized()){
-            kakao.init('1665569fb1877b599992ac86809857fc');
-        }
-
-        kakao.Link.sendDefault({
-            objectType: "feed",
-            content:{
-                title:"카카오톡 링크 보내기 test",
-                description: "...",
-                // imageUrl:''
-                link:{
-                    mobileWebUrl:"https://harin0707.github.io/FE-Project01/",
-                    webUrl:"https://harin0707.github.io/FE-Project01/",
-                }
-            }
+    const handleKakaoLink = ()=>{
+        window.Kakao.Share.createScrapButton({
+            container: "#kakao-link-btn",
+            templateId: 86259,
+            requestUrl: 'https://harin0707.github.io/FE-Project01/'
         })
-
+        console.log('실행중');
     }
+        
 
-
+        
     return (
-    <>
-    <Button onClick={KakaoLink}>
-        공유 버튼
-    </Button>
-    </>
+        <>
+        <button
+        id="kakao-link-btn"
+        type="button"
+        onClick={handleKakaoLink}>
+            카톡 공유
+        </button>
+        </>
     )
 }
 
-const Button = styled.button`
-`
 
-const Img = styled.img`
-`
 
