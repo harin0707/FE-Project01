a2faac3ca53145dbce9938e8938d86e159a4e736
harin0707 <yuharin0707@gmail.com>
2022-11-23 20:27:40 +0900

    fix: env

--- src/component/KakaoButton.js
+++ src/component/KakaoButton.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const kakaoJsKey = process.env.REACT_APP_KAKAO_KEY;
+
 export const KakaoButton = () => {
     //자바스크립트 SDK로 카카오 api 호출
     //카카오 api가 정상적으로 넘어온 것을 확인 가능
     const kakao = window.Kakao;
-    const kakaoJsKey = process.env.REACT_APP_KAKAO_KEY;
     const template = process.env.REACT_APP_MESSAGE_TEMPLATE;
 
     
